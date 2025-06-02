7211adfba0f65fdc6b133f41886283dfee4b489b
LakshithaSenavirathna <srimalsenavirathna44@gmail.com>
2025-06-02 22:18:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,3 +92,4 @@ df_prediction_proba.rename(columns={0: 'Adelie',
                                     1: 'Chinstrap',
                                     2: 'Gentoo'})
 df_prediction_proba   
+df_prediction_proba   

95fa1039a475fdebfb5ace2c47cf9fb074d4d8aa
LakshithaSenavirathna <srimalsenavirathna44@gmail.com>
2025-06-02 22:16:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,3 +76,19 @@ with st.expander('Data preparation'):
   input_row
   st.write('**Encoded y**')   
   y
+
+# Model training and inference
+## Model training
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+## Apply model to make predictions
+prediction = clf.predict(input_row)
+prediction_proba = clf.predict_proba(input_row)
+
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.rename(columns={0: 'Adelie',
+                                    1: 'Chinstrap',
+                                    2: 'Gentoo'})
+df_prediction_proba   
